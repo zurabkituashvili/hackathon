3580aef0b635c91077c0947e4e14bcea332b462b
tianshani_ <amo.cholokava@gmail.com>
2025-03-29 21:37:11 +0400

    Clean up tracked _pycache_ and ignored files

--- hackathon/chatbot/urls.py
+++ hackathon/chatbot/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('api/chat/', views.generate_text, name='generate_chat_response'),
+    path('', views.generate_text, name='generate_chat_response'),
     path('api/get-courses/', views.get_user_courses_data, name='get_user_courses_data'),
     path('api/courses/<int:course_id>/chapters/', views.get_course_chapters, name='get_course_chapters'),
 ]
