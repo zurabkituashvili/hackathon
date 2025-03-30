d90639fc40c4e090e66226488a824d3875d9535f
tianshani_ <amo.cholokava@gmail.com>
2025-03-30 04:39:05 +0400

    mc

--- hackathon/chatbot/models.py
+++ hackathon/chatbot/models.py
@@ -5,9 +5,7 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Name = models.CharField(max_length=255)
-    Description = models.TextField()
     created_at = models.DateField()
-    weeks = models.IntegerField()
 
 class Chapters(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE) # course_id_fk
@@ -21,15 +19,14 @@ class LearningFiles(models.Model):
 
 class ChapterHistory(models.Model):
     chapter = models.ForeignKey(Chapters, on_delete=models.CASCADE) # chapter_id_fk
-    file_uri = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField()
     role = models.CharField(max_length=255)
-    completion_date = models.DateField(null=True, blank=True)
-    number_of_completions = models.IntegerField(null=True, blank=True)
-    first_quiz_date = models.DateField(null=True, blank=True)
 
 class Quiz(models.Model):
     chapter = models.ForeignKey(Chapters, on_delete=models.CASCADE) # chapter_id_fk
     question = models.TextField()
     answer = models.TextField()
     evaluation = models.TextField()
+    completion_date = models.DateField(null=True, blank=True)
+    number_of_completions = models.IntegerField(null=True, blank=True)
+    first_quiz_date = models.DateField(null=True, blank=True)
--- hackathon/chatbot/views.py
+++ hackathon/chatbot/views.py
@@ -1,24 +1,43 @@
+from datetime import date
 import os
+from datetime import date
+
 import dotenv
+# from google import genai
+import google.generativeai as genai2
 import jwt
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
 from django.conf import settings
-from .models import Course, Chapters, ChapterHistory
+from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from google import genai
 from google.genai import types
-import dotenv
+from google.genai.types import ModelContent, UserContent
+from pydantic import BaseModel
+
+from .models import Course, Chapters
+
+
+class GeminiResponse(BaseModel):
+    chapterName: str
+
 
 dotenv.load_dotenv()
+
+chat_sessions = dict()
+
+genai2.configure(api_key=os.getenv('GEMINI_API_KEY'))
+model = genai2.GenerativeModel("gemini-2.0-flash")
+
 
 # Send request to generate answer based on input
 @csrf_exempt
 def generate_text(request):
     if request.method == 'POST':
-        user_input = request.POST.get('user_input', '') # Get user input from the request
+        user_input = request.POST.get('user_input', '')  # Get user input from the request
         try:
             client = genai.Client(
                 api_key=os.getenv("GEMINI_API_KEY"),
@@ -40,9 +59,10 @@ def generate_text(request):
                     ],
                 ),
             ]
-            generate_content_config = types.GenerateContentConfig(
-                response_mime_type="text/plain",
-            )
+            generate_content_config = {
+                'response_mime_type': 'application/json',
+                'response_schema': list[str]
+            }
             response = client.models.generate_content(
                 model=model,
                 contents=contents,
@@ -57,7 +77,8 @@ def generate_text(request):
         except Exception as e:
             return e
     else:
-        return render(request, 'chat_template.html') # Or your API endpoint logic
+        return render(request, 'chat_template.html')  # Or your API endpoint logic
+
 
 @csrf_exempt
 def get_user_courses_data(request):
@@ -70,7 +91,8 @@ def get_user_courses_data(request):
     token = authorization_header[7:]  # Extract the token part
 
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256']) # Replace with your actual secret key and algorithm
+        payload = jwt.decode(token, settings.SECRET_KEY,
+                             algorithms=['HS256'])  # Replace with your actual secret key and algorithm
         user_id = payload.get('user_id')  # Assuming your JWT payload contains 'user_id'
         if not user_id:
             return JsonResponse({'error': 'Invalid JWT payload: user_id not found'}, status=400)
@@ -85,12 +107,11 @@ def get_user_courses_data(request):
 
         for course in courses:
             total_chapters = Chapters.objects.filter(course=course).count()
-            completed_chapters_count = Chapters.objects.filter(comleted=True, course=course).count()
+            completed_chapters_count = Chapters.objects.filter(completed=True, course=course).count()
 
             courses_data.append({
                 'id': course.id,
                 'name': course.Name,
-                'description': course.Description,
                 'total_chapters_count': total_chapters,
                 'completed_chapters_count': completed_chapters_count,
             })
@@ -104,8 +125,8 @@ def get_user_courses_data(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-@csrf_exempt
 
+@csrf_exempt
 def get_course_chapters(request, course_id):
     try:
         course = get_object_or_404(Course, id=course_id)
@@ -116,7 +137,7 @@ def get_course_chapters(request, course_id):
             chapters_data.append({
                 'id': chapter.id,
                 'name': chapter.Name,
-                'completed':chapter.completed,
+                'completed': chapter.completed,
             })
 
         return JsonResponse({'chapters': chapters_data})
@@ -124,3 +145,111 @@ def get_course_chapters(request, course_id):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
+
+@csrf_exempt
+def send_message(request, chapter_id, msg_content):
+    chapter = get_object_or_404(Chapters, id=chapter_id)
+
+    if chapter_id not in chat_sessions:
+        print('should not happen, piiiiiiiiiiizdeeeeeeec')
+        chat_sessions[chapter_id] = model.start_chat(history=os.getenv('SYS_INSTRUCTIONS_1'))
+
+    session = chat_sessions[chapter_id]
+    response = session.send_message(msg_content)
+
+    return JsonResponse({'response': response, 'is_quiz': 0})
+
+
+@csrf_exempt
+def generate_course(request):
+    if request.method == 'POST':
+        file = request.FILES["file"]
+        print(file)
+
+        course_name = request.POST.get('course_name')
+
+        authorization_header = request.META.get('HTTP_AUTHORIZATION')
+        if not authorization_header or not authorization_header.startswith('Bearer '):
+            return JsonResponse({'error': 'Authorization header with JWT token is missing'}, status=401)
+
+        token = authorization_header[7:]  # Extract the token part
+        payload = jwt.decode(token, settings.SECRET_KEY,
+                             algorithms=['HS256'])  # Replace with your actual secret key and algorithm
+        user_id = payload.get('user_id')  # Assuming your JWT payload contains 'user_id'
+        if not user_id:
+            return JsonResponse({'error': 'Invalid JWT payload: user_id not found'}, status=400)
+
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+        if not file:
+            return JsonResponse({'error': 'file pizdu'}, status=404)
+        if not course_name:
+            return JsonResponse({'error': 'course name pizdu'}, status=404)
+
+        client = genai.Client(
+            api_key=os.getenv("GEMINI_API_KEY"),
+        )
+
+        file_name = default_storage.save(file.name, file)
+
+        # fl = default_storage.open(file_name, 'rb')
+
+        filepath = default_storage.path(file_name)
+
+        uploaded_file = client.files.upload(
+            file=filepath
+        )
+        # fl.close()
+
+        system_instructions_2 = os.getenv("SYS_INSTRUCTIONS_2")
+        contents = [
+            types.Part.from_text(text=system_instructions_2),
+            uploaded_file
+        ]
+        generate_content_config = {
+            'response_mime_type': 'application/json',
+            'response_schema': list[GeminiResponse]
+        }
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=contents,
+            config=generate_content_config,
+
+        )
+
+        chapter_names = response.parsed
+
+        course = Course(user=user, Name=course_name, created_at=date.today())
+        course.save()
+
+        res = []
+
+        for chapter_name in chapter_names:
+            chapter = Chapters(course=course, Name=chapter_name.chapterName, completed=False)
+            chapter.save()
+
+            history = [
+                {
+                    'role': 'model',
+                    'parts': [{'text': f'{os.getenv("SYS_INSTRUCTIONS_1")}'}]
+                },
+                {
+                    'role': 'model',
+                    'parts': [uploaded_file]
+                },
+                {
+                    'role': 'model',
+                    'parts': [
+                        {'text': f'This conversation should only cover {chapter_name.chapterName} and nothing else. First ask the user simplest question on this topic/chapter.'}
+                    ]
+                },
+            ]
+
+            chat_sessions[chapter.id] = model.start_chat(history=history)
+            # res.update({'id': chapter.id, 'chapterName': chapter_name})
+            res.append({'id': chapter.id, 'chapterName': chapter_name.chapterName})
+
+        return JsonResponse({'chapters': res})
